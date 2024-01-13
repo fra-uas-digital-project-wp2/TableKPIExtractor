db0972bfca749cde5ccd94cfa13782630be776ce
zjouahri <zakaria.jouahri@capgemini.com>
2024-01-13 17:56:13 +0100

    refactor main_evaluation.py

--- rule_based_pipeline/main_evaluation.py
+++ rule_based_pipeline/main_evaluation.py
@@ -7,34 +7,31 @@
 from TestEvaluation import TestEvaluation
 from KPIResultSet import KPIResultSet
 from globals import print_big
-from PreparationOfKPISpecs import prepare_kpi_specs
 from TestData import TestData
 
 
-def load_test_data(test_data_file_path):
-    test_data = TestData()
-    test_data.load_from_csv(test_data_file_path)
-    return test_data
+def load_actual_values_from_csv(file_path):
+    actual_values = TestData()
+    actual_values.load_from_csv(file_path)
+    return actual_values
 
 
 def evaluation():
-    # Output
-    actual_values = load_test_data(r'output/0_Deutsche Bank_2022_EN.pdf.csv')
+    pdf_file_name = "Deutsche Bank_2018_EN.pdf.csv"
 
-    # output.filter_kpis(by_source_file=['T_Rowe_Price_2021_EN.pdf'])
+    # Output
+    actual_values = load_actual_values_from_csv(r'output/' + pdf_file_name)
 
     print_big("Data-set", False)
     print(actual_values)
 
     # True / Expected Values
-    expected_values = KPIResultSet.load_from_csv(r'output/0_Deutsche Bank_2022_EN.pdf.csv')
+    expected_values = KPIResultSet.load_from_csv(r'output/Deutsche Bank_2018_EN.pdf_3.csv')
     print_big("Kpi-Results", do_wait=False)
     print(expected_values)
 
-    kpis = prepare_kpi_specs()
-
     print_big("Kpi-Evaluation", do_wait=False)
-    test_eval = TestEvaluation.generate_evaluation(expected_values, actual_values)
+    test_eval = TestEvaluation.generate_evaluation(pdf_file_name, expected_values, actual_values)
     print(test_eval)
 
 
