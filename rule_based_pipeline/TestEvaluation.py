618afe6a58f8cfbe9f763003fcd34b9f0b2b2f53
zjouahri <zakaria.jouahri@capgemini.com>
2024-01-13 19:16:18 +0100

    refactor TestEvaluation.py

--- rule_based_pipeline/TestEvaluation.py
+++ rule_based_pipeline/TestEvaluation.py
@@ -9,81 +9,20 @@ from FormatAnalyzer import FormatAnalyzer
 
 
 class TestEvaluation:
+    # Constants for evaluation codes
     EVAL_TRUE_POSITIVE = 0
     EVAL_FALSE_POSITIVE = 1
     EVAL_TRUE_NEGATIVE = 2
     EVAL_FALSE_NEGATIVE = 3
 
-    class TestEvalSample:
-        def __init__(self, kpi_id, kpi_name, pdf_file_name, year, actual_value, expected_value):
-            """
-            Represents a sample for evaluating KPIs.
-
-            Args:
-                kpi_id (KPI): The KPI specification.
-                kpi_name (KPIMeasure): The extracted KPI measure.
-                pdf_file_name (str): The name of the PDF file.
-                year (int): The year associated with the sample.
-                actual_value (TestDataSample): The corresponding test data sample.
-                expected_value (TestDataSample): The corresponding test data sample.
-            """
-            self.kpi_id = kpi_id
-            self.kpi_name = kpi_name
-            self.pdf_file_name = pdf_file_name
-            self.year = year
-            self.actual_value = actual_value
-            self.expected_value = expected_value
-
-        def eval(self):
-            """
-            Evaluate the match between true and extracted values.
-
-            Returns:
-                int: Evaluation code (True Positive, False Positive, True Negative, False Negative).
-            """
-            if self.actual_value is not None and self.expected_value is not None:
-
-                # Check if the absolute difference between extracted and true values is below a threshold
-                if self.actual_value == self.expected_value:
-                    return TestEvaluation.EVAL_TRUE_POSITIVE
-                return TestEvaluation.EVAL_FALSE_POSITIVE
-
-            # No KPI measure, but there is a test sample (False Negative)
-            if self.expected_value is not None:
-                return TestEvaluation.EVAL_FALSE_NEGATIVE
-
-            # No test sample, but there is a KPI measure (False Positive)
-            if self.actual_value is not None:
-                return TestEvaluation.EVAL_FALSE_POSITIVE
-
-            # No KPI measure and no test sample (True Negative) // Unreasonable won't happen in this setup
-            return TestEvaluation.EVAL_TRUE_NEGATIVE
-
-        def eval_to_str(self):
-            """
-            Convert the evaluation result to a human-readable string.
-
-            Returns:
-                str: Human-readable evaluation result.
-            """
-
-            eval_id = self.eval()
-
-            if eval_id == TestEvaluation.EVAL_TRUE_POSITIVE:
-                return "True Positive"
-            if eval_id == TestEvaluation.EVAL_FALSE_POSITIVE:
-                return "False Positive"
-            if eval_id == TestEvaluation.EVAL_TRUE_NEGATIVE:
-                return "True Negative"
-            if eval_id == TestEvaluation.EVAL_FALSE_NEGATIVE:
-                return "False Negative"
-            return "Unknown"
-
     def __init__(self):
         """
         Initialize a TestEvaluation instance.
         """
+        # List to store evaluation samples
         self.eval_samples = []
+
+        # Initialize counters for evaluation metrics
         self.num_true_positive = 0
         self.num_false_positive = 0
         self.num_true_negative = 0
@@ -95,8 +34,8 @@ class TestEvaluation:
         """
         Perform evaluations based on collected samples.
 
-        Updates the evaluation metrics including true positives, false positives,
-        true negatives, false negatives, precision, and recall.
+        Updates the evaluation metrics including true positives, false positives, true negatives, false negatives,
+        precision, and recall.
         """
         # Initialize evaluation counters
         self.num_true_positive = 0
@@ -105,9 +44,10 @@ class TestEvaluation:
         self.num_false_negative = 0
 
         # Iterate through collected samples
-        for eval_sample in self.eval_samples:
-
-            eval_id = eval_sample.eval()
+        for sample in self.eval_samples:
+            actual_value = sample['actual_value']
+            expected_value = sample['expected_value']
+            eval_id = self.eval(actual_value, expected_value)
 
             # Update counters based on evaluation results
             if eval_id == TestEvaluation.EVAL_TRUE_POSITIVE:
@@ -149,11 +89,11 @@ class TestEvaluation:
         console_table.cells.extend(column_headers)
 
         # Populate ConsoleTable with sample data
-        for eval_sample in self.eval_samples:
+        for sample in self.eval_samples:
             console_table.cells.extend([
-                str(eval_sample.kpi_id), str(eval_sample.kpi_name), str(eval_sample.pdf_file_name),
-                str(eval_sample.year), str(eval_sample.actual_value), str(eval_sample.expected_value),
-                eval_sample.eval_to_str().upper()
+                str(sample['kpi_id']), str(sample['kpi_name']), str(sample['pdf_file_name']),
+                str(sample['year']), str(sample['actual_value']), str(sample['expected_value']),
+                self.eval_to_str(sample['actual_value'], sample['expected_value']).upper()
             ])
 
         # Generate formatted string
@@ -169,6 +109,25 @@ class TestEvaluation:
         result += "Recall : " + str(self.measure_recall) + "\n"
 
         return result
+
+    def eval_to_str(self, actual_value, expected_value):
+        """
+        Convert the evaluation result to a human-readable string.
+
+        Returns:
+            str: Human-readable evaluation result.
+        """
+        eval_id = self.eval(actual_value, expected_value)
+
+        if eval_id == TestEvaluation.EVAL_TRUE_POSITIVE:
+            return "True Positive"
+        if eval_id == TestEvaluation.EVAL_FALSE_POSITIVE:
+            return "False Positive"
+        if eval_id == TestEvaluation.EVAL_TRUE_NEGATIVE:
+            return "True Negative"
+        if eval_id == TestEvaluation.EVAL_FALSE_NEGATIVE:
+            return "False Negative"
+        return "Unknown"
 
     def __repr__(self):
         """
@@ -195,30 +154,26 @@ class TestEvaluation:
         results = TestEvaluation()
 
         # Iterate through each actual value
-        for sample in actual_values.samples:
-            # Initialize actual variables
-            actual_kpi_id = int(sample.kpi_id)
-            actual_kpi_name = sample.kpi_name
-            actual_year = int(sample.year)
-            actual_value = sample.value
+        for actual_value in actual_values.samples:
+            # Set flag Match Existence to false
             actual_value_exists_in_expected_values = False
 
             # Iterate through each expected value
-            for expected_value_measure in expected_values.kpi_measures:
-                # Initialize expected variables
-                expected_kpi_id = int(expected_value_measure.kpi_id)
-                expected_year = int(expected_value_measure.year)
-                expected_value = FormatAnalyzer.to_float_number(expected_value_measure.value)
-
+            for expected_value in expected_values.kpi_measures:
                 # Check if the KPI ID and year match between actual and expected values
-                if actual_kpi_id == expected_kpi_id and actual_year == expected_year:
-                    # Remove Matched Value from Expected Values:
-                    expected_values.kpi_measures.remove(expected_value_measure)
+                if actual_value.kpi_id == expected_value.kpi_id and int(actual_value.year) == int(expected_value.year):
+                    # Remove Matched Value from Expected Values
+                    expected_values.kpi_measures.remove(expected_value)
 
                     # Match (Extracted KPI exists)
-                    cur_eval_sample = TestEvaluation.TestEvalSample(actual_kpi_id, actual_kpi_name, pdf_file_name,
-                                                                    actual_year, actual_value, expected_value)
-                    results.eval_samples.append(cur_eval_sample)
+                    results.eval_samples.append({
+                        'kpi_id': int(actual_value.kpi_id),
+                        'kpi_name': actual_value.kpi_name,
+                        'pdf_file_name': pdf_file_name,
+                        'year': int(actual_value.year),
+                        'actual_value': actual_value.value,
+                        'expected_value': FormatAnalyzer.to_float_number(expected_value.value)
+                    })
 
                     # Set flag Match Existence and break
                     actual_value_exists_in_expected_values = True
@@ -226,24 +181,57 @@ class TestEvaluation:
 
             # If no match found for the current actual value, add it as False Negative
             if not actual_value_exists_in_expected_values:
-                cur_eval_sample = TestEvaluation.TestEvalSample(actual_kpi_id, actual_kpi_name, pdf_file_name,
-                                                                actual_year, actual_value, None)
-                results.eval_samples.append(cur_eval_sample)
+                results.eval_samples.append({
+                    'kpi_id': int(actual_value.kpi_id),
+                    'kpi_name': actual_value.kpi_name,
+                    'pdf_file_name': pdf_file_name,
+                    'year': int(actual_value.year),
+                    'actual_value': actual_value.value,
+                    'expected_value': None
+                })
 
         # Add any remaining expected values as False Positive
         if len(expected_values.kpi_measures) > 0:
             for remaining_kpi_measure in expected_values.kpi_measures:
-                # Initialize remaining variables
-                kpi_id = int(remaining_kpi_measure.kpi_id)
-                kpi_name = remaining_kpi_measure.kpi_name
-                year = int(remaining_kpi_measure.year)
-                value = FormatAnalyzer.to_float_number(remaining_kpi_measure.value)
-
-                cur_eval_sample = TestEvaluation.TestEvalSample(kpi_id, kpi_name, pdf_file_name, year, None, value)
-                results.eval_samples.append(cur_eval_sample)
+                results.eval_samples.append({
+                    'kpi_id': int(remaining_kpi_measure.kpi_id),
+                    'kpi_name': remaining_kpi_measure.kpi_name,
+                    'pdf_file_name': pdf_file_name,
+                    'year': int(remaining_kpi_measure.year),
+                    'actual_value': None,
+                    'expected_value': FormatAnalyzer.to_float_number(remaining_kpi_measure.value)
+                })
 
         # Perform evaluations based on collected samples
         results.do_evaluations()
 
         # Return the final evaluation results
         return results
+
+    @staticmethod
+    def eval(actual_value, expected_value):
+        """
+        Evaluate the match between expected (true) and actual (extracted) values.
+
+        Returns:
+            int: Evaluation code (True Positive, False Positive, True Negative, False Negative).
+        """
+        # Check if both actual_value and expected_value are not None
+        if actual_value is not None and expected_value is not None:
+            # Check if the values are equal
+            if actual_value == expected_value:
+                # Return True Positive if they are equal
+                return TestEvaluation.EVAL_TRUE_POSITIVE
+            # Return False Positive if they are not equal
+            return TestEvaluation.EVAL_FALSE_POSITIVE
+
+        # Check if there is an expected value but no actual value and return False Negative
+        if expected_value is not None:
+            return TestEvaluation.EVAL_FALSE_NEGATIVE
+
+        # Check if there is an actual value but no expected value and return False Positive
+        if actual_value is not None:
+            return TestEvaluation.EVAL_FALSE_POSITIVE
+
+        # Unreasonable won't happen in this setup, but if none of the above conditions are met, return True Negative
+        return TestEvaluation.EVAL_TRUE_NEGATIVE
