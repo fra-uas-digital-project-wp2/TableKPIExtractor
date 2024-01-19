881156fb814c1c7b75ad35b94656f32a81ec771f
zjouahri <zakaria.jouahri@capgemini.com>
2024-01-19 22:29:53 +0100

    refactor Parameters.py

--- rule_based_pipeline/Parameters.py
+++ rule_based_pipeline/Parameters.py
@@ -10,33 +10,33 @@
 global_verbosity = 1
 
 global_exec_folder = r"./rule_based_pipeline/"
-
 global_raw_pdf_folder = r"raw_pdf/"
 global_input_folder = r"input/"
 global_working_folder = r"workdir/"
 global_output_folder = r"output/"
 global_expected_values_folder = r"expected_values/"
 
-
-# Filter PDFs and pages
-global_name_of_pdf = "*"
-global_page_of_table_in_pdf = "*"
-
-global_debug_mode = False  # default: False. If true, detected tables are printed and HTMLDirectory is serialized.
-
+# default: False. If true, detected tables are printed and HTMLDirectory is serialized.
+global_debug_mode = False
 
 global_rendering_font_override = r"default_font.otf"
 global_approx_font_name = r"default_font.otf"  # use this font as approximation
 global_max_identify_complex_items_timeout = 0.5  # seconds
 
 global_force_special_items_into_table = True
-global_row_connection_threshold = 10.0  # default=5 . If there is empty space for that many times the previous row height, we will consider this as two distinct tables
-global_be_more_generous_with_good_tables = True  # default=False. If true, we will consider some tables as good that normally considered bad
+# default=5 . If there is empty space for that many times the previous row height, we will consider this as two distinct tables
+global_row_connection_threshold = 10.0
+# default=False. If true, we will consider some tables as good that normally considered bad
+global_be_more_generous_with_good_tables = True
 
-global_table_merge_non_overlapping_rows = True  # default: Fale. If true, system will try to merge non-overlapping rows that probably belong to the same cell
+# default: Fale. If true, system will try to merge non-overlapping rows that probably belong to the same cell
+global_table_merge_non_overlapping_rows = True
 
-global_html_encoding = "utf-8"  # default: "ascii"
+# default: "ascii"
+global_html_encoding = "utf-8"
 
-global_ignore_all_years = False  # default: False. Set it to true to ignore all years for every KPI (this is used for CDP reports)
+# default: False. Set it to true to ignore all years for every KPI (this is used for CDP reports)
+global_ignore_all_years = False
 
-global_analyze_multiple_pages_at_one = False  # default: False. Set it to True, to additionally search for KPIs on multiple (currently: 2) subsequent pages at once.
+# default: False. Set it to True, to additionally search for KPIs on multiple (currently: 2) subsequent pages at once.
+global_analyze_multiple_pages_at_one = False
